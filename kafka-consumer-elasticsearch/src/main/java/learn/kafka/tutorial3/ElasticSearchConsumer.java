e9b512550664e8611f513001db303ca96b1d8eb7
jim <ejavaguy@gmail.com>
2019-10-27 14:02:52 -0400

    finished at-least-once w/o commit

---
+++ kafka-consumer-elasticsearch/src/main/java/learn/kafka/tutorial3/ElasticSearchConsumer.java
@@ -0,0 +1,129 @@
+package learn.kafka.tutorial3;
+
+import com.google.gson.JsonParser;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+@Slf4j
+public class ElasticSearchConsumer {
+    private static String topic="twitter_tweets";
+
+    public static RestHighLevelClient createClient() {
+        Properties props = new Properties();
+        String path = System.getProperty("user.home") + "/.elastic-search.properties";
+        try (FileInputStream fis = new FileInputStream(path)) {
+            props.load(fis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String hostname = props.getProperty("hostname");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+
+        //for remote hosted
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY,
+                    new UsernamePasswordCredentials(username,password));
+
+            RestClientBuilder builder = RestClient.builder(
+                    new HttpHost(hostname, 443, "https"))
+                        .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                            @Override
+                            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                                return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                            }
+                        });
+
+            RestHighLevelClient client = new RestHighLevelClient(builder);
+            return client;
+    }
+
+    public static KafkaConsumer<String, String> createConsumer(String...topics) {
+        String groupdId="kafka-demo-elasticsearch";
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupdId);
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topics));
+        return consumer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (RestHighLevelClient client = createClient();
+             KafkaConsumer<String, String> consumer = createConsumer(topic)) {
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record: records) {
+                    log.info("Key: {}, Value: {}, Partition: {}, Offset: {}",
+                            record.key(), record.value(), record.partition(), record.offset());
+
+                    String json = record.value();
+
+                    //way1 -- generic Kafka ID
+                    //String id = record.topic() + record.partition() + record.offset();
+                    //way2 -- business ID
+                    String id = extractIdFromTweet(record.value());
+
+                    IndexRequest indexRequest = new IndexRequest(
+                            "twitter",
+                            "_doc",
+                            id //to make consumer idempotent
+                    ).source(json, XContentType.JSON);
+                    IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                    log.info("id={}", indexResponse.getId());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static JsonParser jsonParser = new JsonParser();
+    private static String extractIdFromTweet(String tweetJson) {
+        return jsonParser.parse(tweetJson)
+                .getAsJsonObject()
+                .get("id_str")
+                .getAsString();
+    }
+}
+
+/*
+PUT /twitter/tweets/1
+{
+"course":"Kafka for Beginners",
+"module":"ElasticSearch"
+}
+ */
